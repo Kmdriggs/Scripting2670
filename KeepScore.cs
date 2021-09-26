a4487236c88c99cbbafc0b8a263a9a16e72f4eed
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-09-25 22:50:06 -0600

    Keep Score Script

---
+++ KeepScore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeepScore : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public int score = 5;
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    
+}
