15ac97ba0f0df1d4723baf9df93f470edf888ceb
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-09-25 22:50:27 -0600

    Repeat Background Script

---
+++ RepeatBackground.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float speed = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * -1 * Time.deltaTime * speed);
+
+        if (transform.position.z < startPos.z - 25)
+        {
+            transform.position = startPos;
+        }
+    }
+}
