43c09cdd9d21fc0262bbf7d61d0bd74fc82c8e8f
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-09-25 22:48:59 -0600

    Add files via upload

---
+++ MoveDown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDown : MonoBehaviour
+{
+    private Rigidbody objectRb;
+    public float speed = 5.0f;
+    private float zDestroy = -15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectRb.AddForce(Vector3.forward * -speed);
+
+        if(transform.position.z < zDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
