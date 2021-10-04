187e6b3c68c6a1d6f8c7f382836fb97771233969
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-10-04 11:20:43 -0600

    CoroutineBehavior and PoolManager Scripts

---
+++ CoroutineBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineBehavior : MonoBehaviour
+{
+    public UnityEvent startEvent;
+    public bool canRun = true;
+    public float holdTime = 2f;
+    private WaitForSeconds wfs;
+
+    public void StartSimpleCoroutine()
+    {
+        StartCoroutine(routine.CoroutineSimple());
+    }
+
+    private IEnumerator CoroutineSimple()
+    {
+        wfs = new WaitForSeconds(holdTime);
+
+        while (canRun)
+        {
+            yield return wfs;
+            startEvent.Invoke();
+        }
+    }
+
+    public void ChangeWaitTime(float time)
+    {
+        wfs = new WaitForSeconds(time);
+    }
+
+    public void StartCoroutineWithIncrease()
+    {
+        (StartCoroutine(routine.CoroutineWithIncrease());
+    }
+
+    private IEnumerator CoroutineWithIncrease()
+    {
+        wfs = new WaitForSeconds(holdTime);
+
+        while(canRun)
+        {
+            yield return wfs;
+            if (holdTime >= 0.1f)
+            {
+                holdTime -= 0.1f;
+                wfs = new WaitForSeconds(holdTime);
+            }
+            startEvent.Invoke();
+        }
+    }
+}
---
+++ poolManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poolManager : MonoBehaviour
+{
+    public list<GameObject> pool;
+    private int 1;
+
+    public void UseNext()
+    {
+        if(pool.Capacity > 0)
+        {
+            pool[i].SetActive(true);
+            i++;
+        }
+        else
+        {
+            i = 0;
+        }
+    }
+}
