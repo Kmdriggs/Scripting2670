6cbd82651cea806d5ff6f4b9cfc139262b72b204
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-10-04 11:31:16 -0600

    OnEnableCoroutine script

---
+++ OnStartCoroutine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnEnableCoroutine : MonoBehaviour
+{
+    public UnityEvent onEnableEvent;
+    public bool canRun = true;
+    public float holdTime = 2f;
+    private WaitForSeconds wfs;
+
+    private IEnumerator Start()
+    {
+        wfs = new WaitForSeconds(holdTime);
+
+        while (canRun)
+        {
+            yield return wfs;
+            startEvent.Invoke();
+        }
+    }
+}
