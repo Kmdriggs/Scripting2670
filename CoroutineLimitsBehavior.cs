1e7b89f410ddf1345ae5d4aafb1c484b6d190c30
Kimberly Driggs <77745076+Kmdriggs@users.noreply.github.com>
2021-10-20 11:26:11 -0600

    Coroutine limits behavior script

---
+++ CoroutineLimitsBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineLimitsBehavior : MonoBehaviour
+{
+    public float limitNum;
+    private Vector3 positionVector3 = new Vector3();
+
+    private WaitForFixedUpdate wffuObj;
+
+    private void Start()
+    {
+        wffuObj = new WaitForFixedUpdate();
+        positionVector3 =  transform.position;
+        RunCoroutine();
+
+    }
+
+    public void RunCoroutine()
+    {
+        StartCoroutinr(routine:CoroutineWithLimit());
+    }
+
+    private IEnumerator CoroutineWithLimit()
+    {
+        while (transform.position.x < limitNum)
+        {
+            positionVector3.x += 0.1f;
+            transform.Translate(positionVector3)
+            yield return wffuObj;
+        }
+    }
+}
